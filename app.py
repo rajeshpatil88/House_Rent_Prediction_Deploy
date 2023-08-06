9e8f5357a794591dc70d0f290b33f0d50760e2f1
Rajesh Patil <100502337+rajeshpatil88@users.noreply.github.com>
2023-08-06 08:42:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model1.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
